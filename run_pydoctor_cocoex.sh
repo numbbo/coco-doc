304c5d7ba83e5fe27c177339d31a1b912d2554be
nikohansen <nikolaus.hansen@inria.fr>
2025-03-07 23:37:16 +0100

    [api-docs] polish pydoctor script

--- run_pydoctor_cocoex.sh
+++ run_pydoctor_cocoex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# The first argument is the folder where the repository folders 
+# The first argument is the folder where the repository folder
 #   coco-experiment can be found, typically it may be ".."
 
 python "$1"/coco-experiment/scripts/fabricate
@@ -9,12 +9,9 @@ pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/example
 # after checking: apidocs/example/index.html
 echo "  to catch new files execute 'git add apidocs/example'"
 
-# the following requires to have run the build for cocoex via scripts/fabricate (maybe from the folder code-postprocessing)
 mkdir tmp
 cp -r "$1"/coco-experiment/build/python/src/cocoex tmp
 pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/cocoex' tmp/cocoex > err-pydoc-cocoex.txt
-echo "  to catch new files execute 'git add apidocs/cocoex'"
+echo "  to catch new files execute 'git add apidocs'"
+echo "  then execute 'git commit'"
 rm -r tmp
-
-# replaced by the above --html-output='apidocs/example'
-# pydoctor --docformat=restructuredtext --make-html --html-output='apidocs/example' "$1"/code-experiments/build/python/example/example_experiment2.py
